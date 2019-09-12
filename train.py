5c86af8a14429ea6f0a97a73fcd8baf81c9d1cab
Daniel Perez <2cdanielperez@gmail.com>
2019-09-12 13:48:09 -0400

    Log epoch in wandb

--- train.py
+++ train.py
@@ -61,6 +61,7 @@ if __name__ == '__main__':
 
         print('End of epoch %d / %d \t Time Taken: %d sec' %
               (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
+        wandb.log({"Epoch": epoch})
         model.update_learning_rate()
         if opt.verbose_plot:
             writer.plot_model_wts(model, epoch)
