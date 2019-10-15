7c612d80a0b3a107945e88f7ba79b17adce033f7
dpere013 <dpere013@turing1.cm.cluster>
2019-10-15 18:39:52 -0400

    Modify script for shrec6 experiments

--- scripts/shrec/train.sh
+++ scripts/shrec/train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=shrec
-#SBATCH --output=outputs/face_pool2_shrec.txt
+#SBATCH --job-name=s1245
+#SBATCH --output=outputs/shrec6_1-2-4-5.txt
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL
@@ -12,12 +12,12 @@ enable_lmod
 module load python/3.6
 module load cuda/9.2
 module load pytorch/1.0
-module load py-wandb
+module load wandb
 
 ## run the training
 python train.py \
---dataroot datasets/shrec_16 \
---name shrec16 \
+--dataroot datasets/shrec_6 \
+--name shrec6_1-2-4-5 \
 --ncf 64 128 256 256 \
 --pool_res 500 400 300 200 120 \
 --norm group \
@@ -28,4 +28,5 @@ python train.py \
 --niter_decay 100 \
 --gpu_ids 0 \
 --ninput_features 500 \
---feat_from face
+--feat_from face \
+--symm_oper 1 2 4 5
