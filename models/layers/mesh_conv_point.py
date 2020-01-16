ef8d3f7ad618b453b73632200e1786aea9468e3d
Daniel Perez <2cdanielperez@gmail.com>
2020-01-16 11:35:46 -0500

    neighbors in gen with no mem problem

--- models/layers/mesh_conv_point.py
+++ models/layers/mesh_conv_point.py
@@ -8,10 +8,10 @@ class MeshConvPoint(nn.Module):
 
     def __init__(self, in_channels, out_channels, bias=True, symm_oper=None):
         super(MeshConvPoint, self).__init__()
-        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=(1, 1),
-                              bias=bias)
         self.symm_oper = symm_oper
-        self.k = 1
+        self.k = 2
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=(1, self.k),
+                              bias=bias)
 
     def __call__(self, point_f, mesh):
         return self.forward(point_f, mesh)
@@ -20,10 +20,9 @@ class MeshConvPoint(nn.Module):
         # x = x.squeeze(-1)
 
         # build 'neighborhood image' and apply convolution
-        # G = self.create_GeMM(x, mesh)
-
-        x = self.conv(x.unsqueeze(3))
-        return x
+        G = self.create_GeMM(x, mesh)
+        return self.conv(G)
+        # return self.conv(x.unsqueeze(3))
 
 
     def create_GeMM(self, x, mesh):
@@ -33,12 +32,11 @@ class MeshConvPoint(nn.Module):
         output dimensions: Batch x Channels x Vertices x 2
         """
         # TODO: faster way to do this?
-        f = torch.zeros((x.shape[0], x.shape[1], x.shape[2], len(self.symm_oper) + 1), requires_grad=True,
-                        device=x.device)
-        f[:, :, :, 0] = x
+        x = torch.cat((x.unsqueeze(3), torch.ones(x.shape[0], x.shape[1], x.shape[2], 1)),3)
+
         for i in range(mesh.shape[0]):
             for j in range(mesh[i].vs.shape[0]):
                 # Do mean to account for different number of neighbors
-                f[i, :, j, 1] = torch.mean(x[i, :, np.array(list(mesh[i].gemm_vs[j]))], dim=1)
+                x[i, :, j, 1] = torch.mean(x[i, :, np.fromiter(mesh[i].gemm_vs[j], int, len(mesh[i].gemm_vs[j])),0], dim=1)
 
-        return f
+        return x
