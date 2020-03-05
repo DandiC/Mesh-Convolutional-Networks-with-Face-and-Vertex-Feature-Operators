6a5e91a6eed64315c9457db36bcd2b6eeacc62f9
Daniel Perez <2cdanielperez@gmail.com>
2020-03-05 10:18:46 -0500

    make classification the default parameters

--- options/base_options.py
+++ options/base_options.py
@@ -12,43 +12,47 @@ class BaseOptions:
     def initialize(self):
         # PARAMETERS FOR CLASSIFICATION
         # data params
-        # self.parser.add_argument('--dataroot', default='datasets/shrec_16e', help='path to meshes (should have subfolders train, test)')
-        # self.parser.add_argument('--dataset_mode', choices={"classification", "segmentation", "generative"}, default='classification')
-        # self.parser.add_argument('--ninput_features', type=int, default=500, help='# of input features (will include dummy features)')
-        # # network params
-        # self.parser.add_argument('--batch_size', type=int, default=16, help='input batch size')
-        # self.parser.add_argument('--arch', type=str, default='mconvnet',
-        #                          choices={"mconvnet", "meshunet", "meshGAN", "meshPointGAN"},
-        #                          help='selects network to use')
-        # self.parser.add_argument('--resblocks', type=int, default=1, help='# of res blocks')
-        # self.parser.add_argument('--fc_n', type=int, default=100, help='# between fc and nclasses') #todo make generic
-        # self.parser.add_argument('--ncf', nargs='+', default=[128, 256, 256, 512], type=int, help='conv filters')
-        # self.parser.add_argument('--pool_res', nargs='+', default=[500, 400, 300, 200, 120], type=int, help='pooling res')
-        # self.parser.add_argument('--norm', type=str, default='batch',help='instance normalization or batch normalization or group normalization')
-        # self.parser.add_argument('--num_groups', type=int, default=16, help='# of groups for groupnorm')
-        # self.parser.add_argument('--init_type', type=str, default='normal', help='network initialization [normal|xavier|kaiming|orthogonal]')
-        # self.parser.add_argument('--init_gain', type=float, default=0.02, help='scaling factor for normal, xavier and orthogonal.')
-        # self.parser.add_argument('--face_pool', type=str, default='v2', help='Version of face pool. For tracking purposes only.')
-        # self.parser.add_argument('--symm_oper', nargs='+', default=[1], type=int, help='pooling res')
-        # self.parser.add_argument('--vertex_features', nargs='+', type=str, default=['mean_c', 'gaussian_c'],
-        #                          help='Type of vertex features to be used (for vertex convolution). Options are coord (for coordinates), norm (for normals), mean_c (for mean curvature) and gaussian_c (for gaussian curvature)')
-        # self.parser.add_argument('--n_neighbors', default=6, type=int, help='Number of neighbors selected for point (vertex) convolution. If set to -1, network does the average of all vertices')
-        # self.parser.add_argument('--gen_steps', type=int, default=1, help='# of training steps for the generator')
-        # self.parser.add_argument('--disc_steps', type=int, default=1, help='# of training steps for the discriminator')
-        # self.parser.add_argument('--dilation',  default=False, action='store_true',
-        #                          help='Determines if the generator outputs dilation (true) or vertex positions (false)')
-        # # general params
-        # self.parser.add_argument('--feat_from', type=str, default='face', help='Primitive to extract features from. One of: edge, face, point')
-        # self.parser.add_argument('--num_threads', default=3, type=int, help='# threads for loading data')
-        # self.parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
-        # self.parser.add_argument('--name', type=str, default='sweep', help='name of the experiment. It decides where to store samples and models')
-        # self.parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints', help='models are saved here')
-        # self.parser.add_argument('--serial_batches', action='store_true', help='if true, takes meshes in order, otherwise takes them randomly')
-        # self.parser.add_argument('--seed', type=int, help='if specified, uses seed')
-        # self.parser.add_argument('--clean_data',  default=False, action='store_true',
-        #                          help='If true, it deletes the cache NPZ files in the dataset before training.')
+        self.parser.add_argument('--dataroot', default='datasets/shrec_16e', help='path to meshes (should have subfolders train, test)')
+        self.parser.add_argument('--dataset_mode', choices={"classification", "segmentation", "generative"}, default='classification')
+        self.parser.add_argument('--ninput_features', type=int, default=500, help='# of input features (will include dummy features)')
+        self.parser.add_argument('--latent_path', default='datasets/latent/simple_cube.obj',
+                                 help='Path to the OBJ containing the latent for connectivity')
+        # network params
+        self.parser.add_argument('--batch_size', type=int, default=16, help='input batch size')
+        self.parser.add_argument('--arch', type=str, default='mconvnet',
+                                 choices={"mconvnet", "meshunet", "meshGAN", "meshPointGAN"},
+                                 help='selects network to use')
+        self.parser.add_argument('--resblocks', type=int, default=1, help='# of res blocks')
+        self.parser.add_argument('--fc_n', type=int, default=100, help='# between fc and nclasses') #todo make generic
+        self.parser.add_argument('--ncf', nargs='+', default=[128, 256, 256, 512], type=int, help='conv filters')
+        self.parser.add_argument('--pool_res', nargs='+', default=[500, 400, 300, 200, 120], type=int, help='pooling res')
+        self.parser.add_argument('--norm', type=str, default='batch',help='instance normalization or batch normalization or group normalization')
+        self.parser.add_argument('--num_groups', type=int, default=16, help='# of groups for groupnorm')
+        self.parser.add_argument('--init_type', type=str, default='normal', help='network initialization [normal|xavier|kaiming|orthogonal]')
+        self.parser.add_argument('--init_gain', type=float, default=0.02, help='scaling factor for normal, xavier and orthogonal.')
+        self.parser.add_argument('--face_pool', type=str, default='v2', help='Version of face pool. For tracking purposes only.')
+        self.parser.add_argument('--symm_oper', nargs='+', default=[1], type=int, help='pooling res')
+        self.parser.add_argument('--vertex_features', nargs='+', type=str, default=['mean_c', 'gaussian_c'],
+                                 help='Type of vertex features to be used (for vertex convolution). Options are coord (for coordinates), norm (for normals), mean_c (for mean curvature) and gaussian_c (for gaussian curvature)')
+        self.parser.add_argument('--n_neighbors', default=6, type=int, help='Number of neighbors selected for point (vertex) convolution. If set to -1, network does the average of all vertices')
+        self.parser.add_argument('--gen_steps', type=int, default=1, help='# of training steps for the generator')
+        self.parser.add_argument('--disc_steps', type=int, default=1, help='# of training steps for the discriminator')
+        self.parser.add_argument('--dilation',  default=False, action='store_true',
+                                 help='Determines if the generator outputs dilation (true) or vertex positions (false)')
+        self.parser.add_argument('--skip_connections', default=False, action='store_true',
+                                 help='If true, there are skip connections between encoder and decoder')
+        # general params
+        self.parser.add_argument('--feat_from', type=str, default='face', help='Primitive to extract features from. One of: edge, face, point')
+        self.parser.add_argument('--num_threads', default=3, type=int, help='# threads for loading data')
+        self.parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
+        self.parser.add_argument('--name', type=str, default='sweep', help='name of the experiment. It decides where to store samples and models')
+        self.parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints', help='models are saved here')
+        self.parser.add_argument('--serial_batches', action='store_true', help='if true, takes meshes in order, otherwise takes them randomly')
+        self.parser.add_argument('--seed', type=int, help='if specified, uses seed')
+        self.parser.add_argument('--clean_data',  default=False, action='store_true',
+                                 help='If true, it deletes the cache NPZ files in the dataset before training.')
         # # visualization params
-        # self.parser.add_argument('--export_folder', type=str, default='', help='exports intermediate collapses to this folder')
+        self.parser.add_argument('--export_folder', type=str, default='', help='exports intermediate collapses to this folder')
 
         # PARAMETERS FOR SEGMENTATION
         # data params
@@ -250,7 +254,7 @@ class BaseOptions:
                                  help='Primitive to extract features from. One of: edge, face')
         self.parser.add_argument('--num_threads', default=3, type=int, help='# threads for loading data')
         self.parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
-        self.parser.add_argument('--name', type=str, default='autoencoder_cuboids_8v_noRelu_noSkip',
+        self.parser.add_argument('--name', type=str, default='autoencoder_cuboids_8v_noSkip',
                                  help='name of the experiment. It decides where to store samples and models')
         self.parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints', help='models are saved here')
         self.parser.add_argument('--serial_batches', action='store_true',
