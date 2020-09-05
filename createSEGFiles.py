9b66d819d4d9232dba98e9dbcb5f895a4fca27c1
DandiC <2cdanielperez@gmail.com>
2020-09-05 16:30:37 -0400

    Fix bug that produced negative labels

--- createSEGFiles.py
+++ createSEGFiles.py
@@ -5,7 +5,7 @@ import numpy as np
 import os
 
 if __name__ == '__main__':
-    opt = TrainOptions().parse()
+    opt = TestOptions().parse()
     dataset = DataLoader(opt)
 
     sseg_face_folder = os.path.join(opt.dataroot,'sseg_face')
@@ -23,7 +23,7 @@ if __name__ == '__main__':
         os.makedirs(seg_vt_folder)
 
     for i, data in enumerate(dataset):
-        labels = data['label']
+        labels = data['label'] + 1
         soft_labels = data['soft_label']
         for m, mesh in enumerate(data['mesh']):
             mesh_edge_labels = labels[m]
@@ -33,10 +33,11 @@ if __name__ == '__main__':
             mesh_face_soft_labels = np.zeros((mesh.face_count, soft_labels.shape[-1]))
             for f, _ in enumerate(mesh.faces):
                 face_labels = mesh_edge_labels[mesh.edges_in_face[f]]
+                assert 0 not in face_labels
                 uniques, counts = np.unique(face_labels,return_counts=True)
                 mesh_face_labels.append(uniques[np.argmax(counts)].astype(int))
                 for u, unique in enumerate(uniques):
-                    mesh_face_soft_labels[f, int(unique)] = counts[u]/len(face_labels)
+                    mesh_face_soft_labels[f, int(unique)-1] = counts[u]/len(face_labels)
 
             # Save face labels
             np.savetxt(os.path.join(sseg_face_folder, mesh.filename.replace('.obj', '.seseg')), mesh_face_soft_labels,
@@ -49,10 +50,11 @@ if __name__ == '__main__':
             mesh_vs_soft_labels = np.zeros((mesh.vs_count, soft_labels.shape[-1]))
             for v, _ in enumerate(mesh.vs):
                 vs_labels = mesh_edge_labels[mesh.ve[v]]
+                assert 0 not in vs_labels
                 uniques, counts = np.unique(vs_labels, return_counts=True)
                 mesh_vs_labels.append(uniques[np.argmax(counts)].astype(int))
                 for u, unique in enumerate(uniques):
-                    mesh_vs_soft_labels[v, int(unique)] = counts[u] / len(vs_labels)
+                    mesh_vs_soft_labels[v, int(unique)-1] = counts[u] / len(vs_labels)
 
             # Save vertex labels
             np.savetxt(os.path.join(sseg_vt_folder, mesh.filename.replace('.obj', '.seseg')),
