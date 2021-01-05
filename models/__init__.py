1d52ec30e74e5c708ca99299925e4b2daa4acd46
DandiC <2cdanielperez@gmail.com>
2021-01-05 12:47:19 +0100

    fix post-cleaning bug

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,4 @@
 def create_model(opt):
-    from .mesh_classifier import ClassifierModel # todo - get rid of this ?
-    from .mesh_generative import GenerativeModel
-    from .mesh_autoencoder import AutoencoderModel
-    if opt.dataset_mode == "generative":
-        if opt.arch == 'meshunet':
-            model = AutoencoderModel(opt)
-        else:
-            model = GenerativeModel(opt)
-    else:
-        model = ClassifierModel(opt)
+    from .mesh_classifier import ClassifierModel
+    model = ClassifierModel(opt)
     return model
