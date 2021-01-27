5f3e947d0a53fc974fd8eddbaf74da64f17c2d22
DandiC <2cdanielperez@gmail.com>
2021-01-27 16:52:33 -0500

    Delete shrec scripts

--- scripts/shrec/test_vertex.sh
+++ scripts/shrec/test_vertex.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=shrec
-#SBATCH --output=outputs/shrec.txt
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
+#SBATCH --job-name=Test_SHREC
+#SBATCH --output=/home/dpere013/MeshCNN/outputs/shrec.txt
+#SBATCH --partition=main
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=dpere013@odu.edu
 
@@ -12,17 +11,14 @@ enable_lmod
 module load python/3.6
 module load cuda/9.2
 module load pytorch/1.0
-module load wandb
 
-## run the training
-python train.py \
+## run the test and export collapses
+python test.py \
 --dataroot datasets/shrec_16 \
---name shrec16 \
+--name shrec16_non_symetric_neighbors \
 --ncf 64 128 256 256 \
 --pool_res 600 450 300 180 \
 --norm group \
 --resblocks 1 \
---flip_edges 0.2 \
---slide_verts 0.2 \
---num_aug 20 \
---niter_decay 100 \
+--export_folder meshes \
+--gpu_ids -1 \
--- scripts/shrec/train_vertex.sh
+++ scripts/shrec/train_vertex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=shrec10
-#SBATCH --output=outputs/shrec10.txt
+#SBATCH --job-name=s1245
+#SBATCH --output=outputs/shrec6_1-2-4-5.txt
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL
@@ -11,20 +11,22 @@ enable_lmod
 
 module load python/3.6
 module load cuda/9.2
-module load pytorch/1.0
+module load pytorch/1.3
 module load wandb
 
 ## run the training
 python train.py \
---dataroot datasets/shrec_10 \
---name shrec10 \
+--dataroot datasets/shrec_6 \
+--name shrec6_1-2-4-5 \
 --ncf 64 128 256 256 \
---pool_res 600 450 300 180 \
+--pool_res 500 400 300 200 120 \
 --norm group \
 --resblocks 1 \
 --flip_edges 0.2 \
 --slide_verts 0.2 \
 --num_aug 20 \
 --niter_decay 100 \
---feat_from edge \
---ninput_features 750 \
+--gpu_ids 0 \
+--ninput_features 500 \
+--feat_from face \
+--symm_oper 1 2 4 5
